773c629ade0ca14b1c2517d8def5a2f2cfb3b9de
TomGorski23 <115868278+TomGorski23@users.noreply.github.com>
2023-01-14 22:56:53 +0100

    adding routes

--- restapipoprawa/routes/api.php
+++ restapipoprawa/routes/api.php
@@ -6,8 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('people', [PeopleController::class, 'index']);
-Route::get('people/{people}', [PeopleController::class, 'show']);
+Route::get('/Gorski/305361/people', [PeopleController::class, 'index']);
+Route::post('/Gorski/305361/people', [PeopleController::class, 'create']);
+Route::get('/Gorski/305361/people/{people}', [PeopleController::class, 'show']);
+Route::delete('/Gorski/305361/people', [PeopleController::class, 'delete']);
+Route::put('/Gorski/305361/people/{people}', [PeopleController::class, 'update']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
