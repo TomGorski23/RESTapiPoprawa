9a6fe9ec04b1e2e965a0a6a5d0f98bc47c27729e
TomGorski23 <115868278+TomGorski23@users.noreply.github.com>
2023-01-14 01:43:17 +0100

    Update PeopleFactory and seeder

--- restapipoprawa/database/factories/PeopleFactory.php
+++ restapipoprawa/database/factories/PeopleFactory.php
@@ -5,16 +5,10 @@ namespace Database\Factories;
 use App\Models\People;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\People>
- */
+
 class PeopleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition()
     {
 
